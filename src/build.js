6a7cb9f816d65b39ef28a94d749b227cfc4f8d2d
Wassim <wassim@sowlutions.com>
2018-08-12 12:42:09 +0300

    minor fix, v0.3.7

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 const builders = require('./builders')
 
-module.exports = function(...builders) {
-  builders.forEach(builder => {
+module.exports = function(..._builders) {
+  _builders.forEach(builder => {
     if (typeof builder == 'function') {
       builder(this)
       return this
