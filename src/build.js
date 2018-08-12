6796c3cb8bb1d92f1af394da3a235b247ba753bc
Wassim <wassim@sowlutions.com>
2018-08-12 12:36:28 +0300

    build expects multipe builders, v0.3.6

--- src/build.js
+++ src/build.js
@@ -1,12 +1,13 @@
 const builders = require('./builders')
 
-module.exports = function(builder) {
-  if (typeof builder == 'function') {
-    builder(this)
-    return this
-  }
-
-  let name = builder
-  builders.build(this, builder)
+module.exports = function(...builders) {
+  builders.forEach(builder => {
+    if (typeof builder == 'function') {
+      builder(this)
+      return this
+    }
+    let name = builder
+    builders.build(this, builder)
+  })
   return this
 }
